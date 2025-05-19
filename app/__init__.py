d03376a0c3c5b41c570c7aa34d9a8660e544c231
JanderC <janderalexisc@gmail.com>
2025-05-18 23:07:54 -0400

    nuevos camios

--- app/__init__.py
+++ app/__init__.py
@@ -19,34 +19,36 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
 
-    # Configuración 
+    # Configuración
     from config import DevelopmentConfig
     app.config.from_object(DevelopmentConfig)
     
     # Obtener las variables de entorno para la conexión a MongoDB
     mongo_user = os.getenv('MONGO_INITDB_ROOT_USERNAME', 'mongo')
     mongo_password = os.getenv('MONGO_INITDB_ROOT_PASSWORD', 'CvnQaghnRxuwqITNEpeKRilLCNsMZZgU')
-    mongo_host = os.getenv('RAILWAY_PRIVATE_DOMAIN', 'tramway.proxy.rlwy.net')
-    mongo_port = os.getenv('MONGOPORT', 40148)  # El puerto específico de Railway (40148 en tu caso)
+    mongo_host = os.getenv('RAILWAY_PRIVATE_DOMAIN', 'tramway.proxy.rlwy.net')  # Usando el proxy correcto
+    mongo_port = os.getenv('RAILWAY_TCP_PROXY_PORT', 40148)  # El puerto para el proxy
 
     # Construir la URI de conexión usando las variables de entorno
-    uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}"
-
+    uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/?connectTimeoutMS=30000&socketTimeoutMS=30000"
+    
     # Configurar MongoDB
     global mongo_client, db
     try:
         # Establecer la conexión con MongoDB
         mongo_client = MongoClient(uri, server_api=ServerApi('1'))
-        mongo_client.admin.command('ping')
+        mongo_client.admin.command('ping')  # Hacer un ping para verificar la conexión
         db = mongo_client.get_database('emergencia_linea_db')  # Especifica el nombre de tu base de datos aquí
         print("Conexión exitosa a MongoDB!")
     except Exception as e:
         print(f"Error al conectar a MongoDB: {e}")
+        db = None  # Asegurarse de que db sea None si hay un error en la conexión
+        return None  # Si la conexión falla, detener la ejecución de la app
 
     # Inicializar extensiones con la app
     jwt.init_app(app)
     CORS(app)
-    
+
     # Registrar blueprints
     from app.routes.auth import auth_bp
     app.register_blueprint(auth_bp)
